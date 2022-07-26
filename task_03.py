e9bae7290f5ffbf5023ed17bdf619bb5414ab79d
Tamara <tomritol@gmail.com>
2022-07-26 22:24:34 +0300

    Задача - найти сумму n чисел последовательности (1+1/n)

--- task_03.py
+++ task_03.py
@@ -2,16 +2,15 @@
 # (1+1\n)^n и выведите на экран их сумму.
 
 def find_lim (n: int):
-    result = 0
+    res = 0
     sum = 0
     count = 0
     for i in range (1, n+1):
-        result = (1 + 1/i)**i
+        res = (1 + 1/i)**i
         count += 1
-        print(f"{count}: {round(result, 2)}")
-        sum += result
+        print(f"{count}: {round(res, 2)}")
+        sum += res
     return(round(sum, 2))
-        
-       
+
 n = int(input("Введите число - "))
 print(f'Сумма чисел списка равна - {find_lim(n)}')
