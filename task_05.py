b388019e5e92c55df8c7232c14d557b9feee74c6
Tamara <tomritol@gmail.com>
2022-07-26 23:40:43 +0300

    Задача - перемешивание списка

---
+++ task_05.py
@@ -0,0 +1,7 @@
+# Реализуйте алгоритм перемешивания списка.
+
+list_01 = list(range(10)) 
+print(f'исходный список {list_01}')
+import random
+random.shuffle(list_01)
+print(f'перемешенный список {list_01}')
